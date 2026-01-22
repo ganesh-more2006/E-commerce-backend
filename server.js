ae3b57672da1e5820ab312f98ecc6f26a23e2a90
ganesh-more2006 <moreganesh855197@gmail.com>
2026-01-22 15:25:15 +0530

    solve error

--- server.js
+++ server.js
@@ -10,11 +10,9 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-  origin: "*", 
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  credentials: true
-}));
+
+app.use(cors()); 
+app.options('*', cors());
 app.use(express.json()); 
 
 app.use('/api/products', productRoutes);
