5f97093577eae00eee5867e3182da25c4bbef07a
ganesh-more2006 <moreganesh855197@gmail.com>
2026-01-22 15:22:17 +0530

    Final CORS fix with headers

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ const app = express();
 app.use(cors({
   origin: "*", 
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
 app.use(express.json()); 
